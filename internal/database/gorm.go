b1dccc33129b09fe6e352e932672b859b2b96f9e
Thalles Passos <contato@thalles.me>
2023-11-06 09:23:02 -0300

    Add database instance caching to improve performance

--- internal/database/gorm.go
+++ internal/database/gorm.go
@@ -7,18 +7,26 @@ import (
 	"gorm.io/gorm"
 )
 
+var dbInstance *gorm.DB
+
 func GetClientOrPanic() *gorm.DB {
+	if dbInstance != nil {
+		return dbInstance
+	}
+
 	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_DSN")), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	err = db.AutoMigrate(&TweetModel{}, &OpenAccountModel{})
+	dbInstance = db
+
+	err = dbInstance.AutoMigrate(&TweetModel{}, &OpenAccountModel{})
 
 	if err != nil {
 		panic("failed to migrate database")
 	}
 
-	return db
+	return dbInstance
 }
